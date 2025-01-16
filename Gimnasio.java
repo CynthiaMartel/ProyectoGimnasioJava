f1b8bb06d6136e1e58de89d90a34decd8d1ca2eb
Cynthia Martel <cynthiamartelmartin@gmail.com>
2025-01-16 16:39:13 +0000

    Creación de la clase Usuario y almacenamiento de los datos de estos. Creación de una función para mostrar la información de los usuarios. Implementación de una clave de acceso como información extra. Implementación de una función para buscar mediante clave a los usuarios y mostrar la información correspondiente, con la interacción por teclado, al introducir dicha clave.

--- Gimnasio.java
+++ Gimnasio.java
@@ -1,25 +1,93 @@
-public class Gimnasio {
-    private String nombre;
-    private String[] actividades = {"Yoga", "Boxeo", "Funcional"};
+//Importamos la clase Scanner para utilizar la lectura de introducción de datos por parte del usuario en consola
+//Importamos la clase de ArrayList para alamacenar información del usuario
+import java.util.Scanner;
+import java.util.ArrayList;
 
-    public Gimnasio(String nombre) {
+
+// Añadimos clase Usuario para almacenar la información del usuario
+class Usuario {
+    String nombre;
+    int claveAcceso;
+    String [] actividades;
+
+    public Usuario(String nombre, int claveAcceso, String[] actividades){
         this.nombre = nombre;
+        this.claveAcceso = claveAcceso;
+        this.actividades = actividades;
     }
+    //Añadimos una función para mostrar información del usuario
+    public void mostrarInfoUsuario(String nombreGimnasio){
+        System.out.println("--Su información--");
+        System.out.println("Usuario: " + nombre) ;
+        System.out.println("Clave de Acceso: " + claveAcceso);
+        System.out.println("Su gimnasio: " + nombreGimnasio);
+        System.out.println("Actividades a las que asistes: ");{
+            for (String actividad: actividades){
+                System.out.print(actividad + " - ");
+            }
+        System.out.println();
+        }
+    }
+}
+public class Gimnasio {
+    //Cambiamos "nombre" por nombreGimnasio", para evitar posibles confusiones entre el nombre de usuario y nombre del gimnasio
+    private String nombreGimnasio;
+    //Añadimos más actividades al gimnasio: "Spinning, Crossfit, Pilates, Acceso libre musculación "
+    private String[] actividades = {"Yoga", "Boxeo", "Funcional", "Sppining", "Crossfit", "Pilates", "Acceso libre musculación"};
+    // Añadimos ArrayList para almacenar a los usuarios
+    private ArrayList<Usuario> usuarios;
 
+    public Gimnasio(String nombreGimnasio) {
+        this.nombreGimnasio = nombreGimnasio;
+        this.usuarios = new ArrayList<>();
+    }
+    
     public void mostrarActividades() {
-        System.out.println("Actividades disponibles en el gimnasio " + nombre + ":");
+        System.out.println("Actividades disponibles en el gimnasio " + nombreGimnasio + ":");
         for (String actividad : actividades) {
             System.out.println("- " + actividad);
         }
     }
-
-    public void registrarUsuario(String usuario) {
-        System.out.println("Usuario " + usuario + " registrado en el gimnasio " + nombre + ".");
+    
+    // Añadimos int clave de acceso, para mostrar la clave de acceso del usuario en la información y utilizarla más abajo para que el usuario pueda acceder a la información con ella
+    public void registrarUsuario(String usuario, int claveAcceso, String[] actividades) {
+        //Creamos el objeto de nuevoUsuario
+        Usuario nuevoUsiario = new Usuario(usuario, claveAcceso, actividades);
+        usuarios.add(nuevoUsiario);
     }
-
-    public static void main(String[] args) {
+    
+    // Añadimos una función que se encargue de buscar la clave de acceso de los usuarios registrados para mostrar la información correspondiente
+    public void buscarClave(int claveAcceso){
+        boolean encontrado = false;
+        for (Usuario usuario: usuarios){
+            if (usuario.claveAcceso == claveAcceso){
+                usuario.mostrarInfoUsuario(nombreGimnasio);
+                encontrado = true;
+                break;
+            }
+        }
+        if (!encontrado){
+            System.out.println("Clave de acceso no encontrada");
+            
+        }
+    }
+        public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+          
+    
         Gimnasio miGimnasio = new Gimnasio("FitLife Gym");
+        // Creamos a 4 usuarios de ejemplo
+        miGimnasio.registrarUsuario("Juan Perez", 1090, new String[]{"Yoga", "Sppining"});
+        miGimnasio.registrarUsuario("María Suárez", 1091, new String[]{"Funcional", "Acceso libre musculación"});
+        miGimnasio.registrarUsuario("Carla Mendoza", 1092, new String[]{"Crossfit", "Boxeo"});
+        miGimnasio.registrarUsuario("Nauzet Carbacho", 1093, new String[]{"Sppining", "Funcional", "Acceso libre musculación"});
+        
         miGimnasio.mostrarActividades();
-        miGimnasio.registrarUsuario("Juan Perez");
+
+        // Añadimos que el usuario pueda acceder a su información con la introducción de DNI
+        System.out.println("Introduzca su clave de acceso para mostrar información: ");
+        int claveAcceso = s.nextInt();
+        miGimnasio.buscarClave(claveAcceso);  
     }
 }
+
--- README.md
+++ README.md
@@ -1,28 +1,30 @@
-# ProyectoGimnasioJava
-Repositorio base para actividad práctica con GitHub y Java
+# Descripción
+Este proyecto simula la gestión de usuarios en un gimnasio. Inicialmente, mi intención era que se añadiesen nuevas actividades al array de actividades como"Spinning", "Crossfit", etc, y que, en el registro de usuario, se añadiese una clave asociada a cada uno de ellos. Posteriormente, quería crear una función que mostrase la información de todos los usuarios registrados. Pero, finalmente, decidí que las mejoras realizadas se centren en permitir a los usuarios acceder a su información personal mediante una clave de acceso, introducida por teclado, aumentando así, la complejidad y la utilidad del programa.
 
-# Gimnasio en Java
-Este repositorio contiene una clase base para gestionar actividades en un gimnasio. Deben realizar un fork y mejorar el proyecto.
+# Mejoras realizadas
+1. Clase Usuario
+-Se creó una nueva clase Usuario para gestionar la información de cada usuario para almacenarla posteriormente.
+-La clase almacena el nombre del usuario, su clave de acceso y las actividades a las que se haya registrado.
 
-## Instrucciones
-1. Realiza un fork de este repositorio.
-2. Añade tus mejoras al proyecto.Las mejoras a valorar son añadir nuevos métodos que aporte un extra a la clase principal.
-3. Crear un archivo `README.md` en tu repositorio forkeado. (ver el punto 5, en criterios de evaluación)
-## Criterios de Evaluación 
-1. Tu cuenta de GitHub: Tu perfil debe estar configurado
-2. Realizar correctamente el fork del repositorio.
-3. En cuanto a la tarea una vez realizado tu FORK:
-4. El código modificado debe incluir al menos una mejora en forma de nuevas clases o métodos.
-Los cambios deben estar claramente visibles en el repositorio forkeado.
-5. En tu FORK debes crear un fichero (README.md):
-El repositorio forkeado debe incluir un archivo README.md que tenga la siguiente estructura 
-Descripción general de las mejoras.
-Justificación de los cambios realizados.
-Instrucciones claras para entender y usar las nuevas funcionalidades.
-Uso de GitHub:
-Las modificaciones deben incluir al menos un commit por cada mejora realizada.
-El commit debe estar documentado con mensajes claros.
-## Entrega:
-En la entrega en la plataforma:  Pega el enlace para que me dirija directamente al FORK, (no les pedire que en el FORK añadan su nombre ya que el repositorio es publico y seguramente la mayoria tendrá un perfil
-con un Pseudonimo, por esta razón prefiero que en la entrega añadan el enclace y así evaluar.
-## Nota: No será necesario hacer pull request.
+2. Almacenamiento Dinámico de Usuarios
+-Se implementó un ArrayList<Usuario> para almacenar múltiples usuarios.
+
+3. Acceso a Información del usuario mediante una función que utiliza la clave de acceso asignada
+-Se añadió un método buscarClave(int claveAcceso) para permitir a los usuarios consultar su información ingresando su clave de acceso.
+
+4. Ampliación de las actividades disponibles en el gimnasio
+-Se amplió la lista de actividades del gimnasio.
+
+# Justificación de los Cambios
+-La mejora en la organización de datos: La creación de la clase Usuario mejora la estructura del código y facilita como se gestiona la información de los usuarios
+-La adaptabilidad y eficacia para registrar nuevos usuarios: Se usa un ArrayList que permite agregar y gestionar múltiples usuarios
+-Funcionalidad ampliada e interacción con el usuario-cliente del programa mediante el ingreso por teclado de su clave de acceso: Se permite el acceso a la información personal mediante una clave para hacer que el programa sea más interactivo y dinámico
+
+# Instrucciones de Uso
+1. Compilar y ejecutar el programa en java
+2. Las actividades disponibles, ya están predefinidas en un array de tipo String. De la misma manera, existen ya creados, 4 usuarios con su nombre, clave de acceso, nombre del gimnasio y actividades en las que participa
+3. El programa mostará primeramente, las actividades disponibles
+4. Seguidamente, mediante la interacción en consola, se solicitará al usuario que introduzca su clave de acceso
+5. Si la clave de acceso es correcta, se le mostrará al usuario, su información correspondiente
+6. Si la clave no es válida, se le mostrará un mensaje de error
+
